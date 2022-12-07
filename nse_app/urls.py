3347b59b6a4d5f70adedf96d63663063771847d2
paras <paras@sopeonow.one>
2022-12-07 12:42:38 +0530

    nishant

--- nse_app/urls.py
+++ nse_app/urls.py
@@ -4,9 +4,24 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = 'home'),
-    path('stocks', views.stock_details.as_view(), name = 'stock_detail'), 
+    path('stocks', views.stock_details.as_view(), name = 'stock_detail'),
+    
+     
     path('setting_nse', views.setting_nse.as_view(), name = 'setting_nse'), 
 
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
     path("get_setting_data/<int:pk>", views.SnippetDetail.as_view(), name="setting_nse"),
     path('delete_stock/<int:pk>', views.delete_stock, name = 'delete-stock'),
     path('patch_stock/<int:pk>', views.patch_stock, name = 'patch-stock'),
